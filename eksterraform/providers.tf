7e343a4a7251f75b35e927a3c0e2c2ebeb690b58
mchidambaram1990 <58851142+mchidambaram1990@users.noreply.github.com>
2020-09-03 08:48:38 +1000

    updated the comments

--- eksterraform/providers.tf
+++ eksterraform/providers.tf
@@ -5,8 +5,6 @@ provider "aws" {
   version = ">= 2.38.0"
 }
 
-# Using these data sources allows the configuration to be generic for any region.
-# data "aws_region" "current" {}
 
 data "aws_availability_zones" "available" {}
 
